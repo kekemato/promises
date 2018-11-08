e09d1374109218b15c283d69e3d644aa09bee88f
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-08 09:50:47 +0100

    made render functions

--- mini-app/script.js
+++ mini-app/script.js
@@ -1,15 +1,55 @@
 class App {
+    constructor() {
+        this.listView = new ListView()
+        this.userView = new UserView()
+        this.notFoundView = new NotFoundView()
 
+    }
+
+    renderView(viewName) {
+        switch (viewName) {
+            case 'listView':
+                this.render(this.listView.render())
+                break
+            case 'userView':
+                this.render(this.userView.render())
+                break
+            case 'notFoundView':
+                this.render(this.notFoundView.render())
+                break
+        }
+    }
+
+    render(viewContent) {
+        document.body.innerHTML = ''
+        document.body.appendChild(viewContent)
+    }
 }
 
-class ListView {
 
+class ListView {
+    render() {
+        const div = document.createElement('div')
+        div.innerText = 'ListView'
+        return div
+    }
 }
 
 class UserView {
-
+    render() {
+        const div = document.createElement('div')
+        div.innerText = 'UserView'
+        return div
+    }
 }
 
 class NotFoundView {
-
+    render() {
+        const div = document.createElement('div')
+        div.innerText = 'NotFoundView'
+        return div
+    }
 }
+
+const app = new App()
+app.renderView('listView')
